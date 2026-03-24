dc8ff027a1c41eb03a6829981440d0ace601cda3
PatMir04 <66876552+PatMir04@users.noreply.github.com>
2026-03-24 14:28:16 -0500

    Add BusinessPhoto admin with inline support

--- businesses/admin.py
+++ businesses/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Category, Business
+from .models import Category, Business, BusinessPhoto
+
+
+class BusinessPhotoInline(admin.TabularInline):
+    model = BusinessPhoto
+    extra = 1
+    readonly_fields = ('uploaded_at',)
 
 
 @admin.register(Category)
@@ -15,3 +21,11 @@ class BusinessAdmin(admin.ModelAdmin):
     search_fields = ['name', 'description', 'address']
     prepopulated_fields = {'slug': ('name',)}
     list_editable = ['is_verified', 'is_active']
+    inlines = [BusinessPhotoInline]
+
+
+@admin.register(BusinessPhoto)
+class BusinessPhotoAdmin(admin.ModelAdmin):
+    list_display = ['business', 'caption', 'is_primary', 'uploaded_at']
+    list_filter = ['is_primary', 'uploaded_at']
+    search_fields = ['business__name', 'caption']
