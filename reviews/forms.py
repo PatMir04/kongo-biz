bdc970daff49d68b78f9ddf5f9fb05f419eb7a09
PatMir04 <66876552+PatMir04@users.noreply.github.com>
2026-03-24 14:30:23 -0500

    Add ReviewFlagForm and BusinessFlagForm

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Review
+from .models import Review, ReviewFlag, BusinessFlag
 
 
 class ReviewForm(forms.ModelForm):
@@ -17,4 +17,40 @@ class ReviewForm(forms.ModelForm):
         labels = {
             'rating': 'Note',
             'comment': 'Commentaire',
+        }
+
+
+class ReviewFlagForm(forms.ModelForm):
+    class Meta:
+        model = ReviewFlag
+        fields = ('reason', 'description')
+        widgets = {
+            'reason': forms.Select(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Decrivez le probleme...'
+            }),
+        }
+        labels = {
+            'reason': 'Raison du signalement',
+            'description': 'Details',
+        }
+
+
+class BusinessFlagForm(forms.ModelForm):
+    class Meta:
+        model = BusinessFlag
+        fields = ('reason', 'description')
+        widgets = {
+            'reason': forms.Select(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Decrivez le probleme...'
+            }),
+        }
+        labels = {
+            'reason': 'Raison du signalement',
+            'description': 'Details',
         }
