dbbd36d317ec76538684c0847f5f055f7689a8dc
PatMir04 <66876552+PatMir04@users.noreply.github.com>
2026-03-24 13:54:54 -0500

    Add seed data: categories and superuser setup

--- build.sh
+++ build.sh
@@ -7,3 +7,22 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations businesses accounts reviews
 python manage.py migrate
+
+# Seed categories and make PatMir superuser
+python manage.py shell << 'SEED'
+from businesses.models import Category
+from django.contrib.auth import get_user_model
+User = get_user_model()
+cats = ['Restaurants','Hotels','Pharmacies','Hopitaux & Cliniques','Ecoles & Universites','Marches','Transport','Banques & Finance','Salons de Beaute','Reparation Auto','Technologie & Telecom','Immobilier','Services Juridiques','Alimentation & Boissons','Divertissement']
+for c in cats:
+    Category.objects.get_or_create(name=c)
+print(f'Categories: {Category.objects.count()}')
+try:
+    u = User.objects.get(username='PatMir')
+    u.is_staff = True
+    u.is_superuser = True
+    u.save()
+    print('PatMir is now superuser')
+except User.DoesNotExist:
+    print('PatMir not found')
+SEED
