a75679c04c2cf66a2b1d49dc7218def1e9a4393f
PatMir04 <66876552+PatMir04@users.noreply.github.com>
2026-03-24 14:33:17 -0500

    Add photo and flagging URL routes

--- businesses/urls.py
+++ businesses/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('categorie/<slug:slug>/', views.category_detail, name='category_detail'),
     path('<slug:slug>/', views.business_detail, name='detail'),
     path('<slug:slug>/modifier/', views.business_edit, name='edit'),
+    path('<slug:slug>/ajouter-photo/', views.add_photo, name='add_photo'),
+    path('<slug:slug>/signaler/', views.flag_business, name='flag_business'),
+    path('photo/<int:photo_id>/supprimer/', views.delete_photo, name='delete_photo'),
 ]
